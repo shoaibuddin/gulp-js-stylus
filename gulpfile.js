a1c14803a36c73c474398a3c0b3f15b4c2ee9a46
Shoaib Ud-Din <suddin@cars.com>
2015-02-14 10:35:29 -0600

    added css minifify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var stylus = require('gulp-stylus');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var minifyCSS = require('gulp-minify-css');
 
 // Scripts task to compile JS files
 gulp.task('scripts', function () {
@@ -11,15 +14,37 @@ gulp.task('scripts', function () {
 		.pipe(gulp.dest('dist/js'));
 });
 
-// Stylus task to compile stylus files into CSS
+// Stylus and minifyCSS task to compile stylus files into CSS
 gulp.task('stylus', function () {
-	gulp.src('src/styl/**/*.styl')
-		.pipe(stylus())
-		.pipe(gulp.dest('dist/css'));
+  gulp.src('src/styl/**/*.styl')
+    .pipe(stylus())
+    //.pipe(minifyCSS({keepBreaks:true}))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist/css'));
+});
+
+// minify-css task to compile stylus files into CSS
+gulp.task('minify-css', function() {
+  gulp.src('dist/css/*.css')
+    //.pipe(minifyCSS({keepBreaks:true}))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./dist/css'))
+});
+
+// Stylus task to compile stylus files into CSS
+gulp.task('imgminify', function () {
+	gulp.src('src/images/*')
+		.pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant({ quality: '20-50', speed: 4 })]
+        }))
+		.pipe(gulp.dest('dist/images'));
 });
 
 // Gulp Watch
-gulp.task('watch', ['scripts','stylus'], function() {
+gulp.task('watch', ['scripts','stylus','imgminify'], function() {
   gulp.watch('./**/*.styl', ['stylus']);
   gulp.watch('./**/*.js', ['scripts']);
+  gulp.watch('./**/*', ['imgminify']);
 });
